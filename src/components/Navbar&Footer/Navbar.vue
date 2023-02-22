c55c2569c0c4b2dae49c1e30491494a10c74af18
niko-u <niko@striker2k.com>
2023-02-21 21:19:50 -0600

    removed items from Navbar and Footer

--- src/components/Navbar&Footer/Navbar.vue
+++ src/components/Navbar&Footer/Navbar.vue
@@ -72,13 +72,7 @@ const logout = () => {
           </div>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button
-            type="button"
-            class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            <span class="sr-only">View notifications</span>
-            <BellIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
+
 
           <!-- Profile dropdown -->
           <Menu v-if="user.currentUser" as="div" class="relative ml-3">
