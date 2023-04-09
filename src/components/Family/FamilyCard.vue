42b15fd288c8bf0b5bef99e55016ebdce7df2b81
niko-u <niko@striker2k.com>
2023-04-08 19:37:39 -0500

    added leave family confirmation modal

--- src/components/Family/FamilyCard.vue
+++ src/components/Family/FamilyCard.vue
@@ -221,8 +221,62 @@ async function leaveFamily() {
               </TransitionRoot>
             </div>
           </button>
-          <button v-else @click="leaveFamily" class="text-white bg-red-500 hover:bg-red-600  py-2 px-4 rounded-xl mr-2 hover:scale-102 transition-all ease-out duration-200 cursor-pointer">
+          <button v-else @click="openModal" class="text-white bg-red-500 hover:bg-red-600  py-2 px-4 rounded-xl mr-2 hover:scale-102 transition-all ease-out duration-200 cursor-pointer">
             Leave
+            <div>
+              <TransitionRoot appear :show="isOpen" as="template">
+                <Dialog as="div" class="relative z-10" @close="closeModal">
+                  <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                  >
+                    <div class="fixed inset-0 bg-black bg-opacity-25" />
+                  </TransitionChild>
+
+                  <div class="fixed inset-0 overflow-y-auto">
+                    <div
+                      class="flex min-h-full items-center justify-center p-4 text-center"
+                    >
+                      <TransitionChild
+                        as="template"
+                        enter="duration-300 ease-out"
+                        enter-from="opacity-0 scale-95"
+                        enter-to="opacity-100 scale-100"
+                        leave="duration-200 ease-in"
+                        leave-from="opacity-100 scale-100"
+                        leave-to="opacity-0 scale-95"
+                      >
+                        <DialogPanel
+                          class="w-full max-w-sm transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                        >
+                          <DialogTitle
+                            as="h3"
+                            class="text-center text-2xl font-bold leading-6 text-gray-900"
+                          >
+                            Are you sure you would like to leave?
+                          </DialogTitle>
+                          <div class="mt-5">
+                            <div class="flex justify-center">
+                              <button @click="leaveFamily" class="w-20 mr-1 bg-gradient-to-r from-green-600 to-green-800 rounded-md hover:from-green-800  hover:to-green-900 text-white font-bold py-2 px-4 rounded shadow-lg">
+                              Yes
+                            </button>
+                            <button  @click="closeModal" class="w-20 mr-1 bg-gradient-to-r from-red-600 to-red-800 rounded-md hover:from-red-800  hover:to-red-900 text-white font-bold py-2 px-4 rounded shadow-lg">
+                              No
+                            </button>
+                            </div>
+                          </div>
+                        </DialogPanel>
+                      </TransitionChild>
+                    </div>
+                  </div>
+                </Dialog>
+              </TransitionRoot>
+            </div>
           </button>
           <button class="bg-gray-300 text-black font-bold rounded-xl shadow-md py-2 px-4 hover:bg-gray-400 hover:scale-102 transition-all ease-out duration-200 cursor-pointer" 
           @click="copyToClipboard(family.code)">
